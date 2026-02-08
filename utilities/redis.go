2cbd47bc48e2ba99e848ac878802b4165acfeeac
nyein phyo aung <146245393+Its-Nyein@users.noreply.github.com>
2026-02-08 14:50:46 +0630

    chore: temporary skip redis key value instance on render

--- utilities/redis.go
+++ utilities/redis.go
@@ -2,22 +2,34 @@ package utilities
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"po-backend/configs"
 )
 
+var errRedisNotConnected = errors.New("redis not connected")
+
 func RedisSet(key string, value interface{}, expiration time.Duration) error {
+	if configs.Envs.Redis == nil {
+		return errRedisNotConnected
+	}
 	ctx := context.Background()
 	return configs.Envs.Redis.Set(ctx, key, value, expiration).Err()
 }
 
 func RedisGet(key string) (string, error) {
+	if configs.Envs.Redis == nil {
+		return "", errRedisNotConnected
+	}
 	ctx := context.Background()
 	return configs.Envs.Redis.Get(ctx, key).Result()
 }
 
 func RedisDel(key string) error {
+	if configs.Envs.Redis == nil {
+		return errRedisNotConnected
+	}
 	ctx := context.Background()
 	return configs.Envs.Redis.Del(ctx, key).Err()
 }
